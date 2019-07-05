2b4ab6eefc28d489579b1491dcfb6d3b0f4f20b7
MartinBFFan <34067979+MartinBFFan@users.noreply.github.com>
2019-07-05 17:15:20 +0200

    Update H2MOD.cpp

--- xlive/H2MOD.cpp
+++ xlive/H2MOD.cpp
@@ -5,6 +5,7 @@
 #include "H2MOD\Discord\DiscordInterface.h"
 #include "H2MOD\Modules\OnScreenDebug\OnscreenDebug.h"
 #include "H2MOD\Modules\HitFix\Hitfix.h"
+#include "H2MOD\Modules\MapFix\SPMapfix.h"
 #include "H2MOD\Modules\MapFix\MPMapfix.h"
 #include "H2MOD\Modules\Input\Mouseinput.h"
 #include "H2MOD\Modules\Tweaks\Tweaks.h"
@@ -1190,6 +1191,7 @@ void __cdecl OnMapLoad(int a1)
 		H2Tweaks::setCrosshairPos(H2Config_crosshair_offset);
 		H2Tweaks::enable60FPSCutscenes();
 		H2Tweaks::setSavedSens();
+		SPMapFix::Initialize();
 	}
 
 	p_set_random_number(a1);
